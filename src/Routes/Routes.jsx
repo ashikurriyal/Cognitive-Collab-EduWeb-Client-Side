4d8732677059f6f8ff8444bb6236a3292351dc6b
ashikurriyal <riyal.ashikur@gmail.com>
2024-05-14 10:07:31 +0600

    Implemented the login and register of a user with firebase authentication

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Home from "../Home/Home";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 
 const router = createBrowserRouter([
@@ -19,7 +20,8 @@ const router = createBrowserRouter([
                 element: <Login></Login>
             },
             {
-                
+                path: '/register',
+                element:<Register></Register>
             }
         ]
     },
