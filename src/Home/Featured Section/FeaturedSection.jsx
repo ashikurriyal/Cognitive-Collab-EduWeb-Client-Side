7da0e41ccb63c933591952472e104a5b14d7aedc
ashikurriyal <riyal.ashikur@gmail.com>
2024-05-15 20:38:18 +0600

    Updated the featured section card with its design and data

--- src/Home/Featured Section/FeaturedSection.jsx
+++ src/Home/Featured Section/FeaturedSection.jsx
@@ -2,9 +2,29 @@
 
 const FeaturedSection = () => {
     return (
-        <div className="max-w-7xl mx-auto text-center space-y-4">
-            <h1 className="text-5xl font-bold">Featured Section</h1>
-            <p className="text-2xl font-medium">Empowering Collaborative Learning: Explore Our Key Features</p>
+        <div className="max-w-7xl mx-auto space-y-10">
+            <div className="text-center space-y-4">
+                <h1 className="text-5xl font-bold">Featured Section</h1>
+                <p className="text-2xl font-medium">Empowering Collaborative Learning: Explore Our Key Features</p>
+            </div>
+            <div className="max-w-sm bg-white border-2 border-fuchsia-600 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl">
+                <a href="#">
+                    <img className="rounded-t-lg" src="https://i.ibb.co/nPXKdHL/ezgif-com-webp-to-png-converter-1.png" alt="" />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Engage in Dynamic Study Circles</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Join or create study groups with your friends for interactive learning sessions. Collaborate on assignments, discuss topics, and share resources in real-time. Enhance your understanding through active participation and collective knowledge sharing.</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Read more
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+
         </div>
     );
 };
